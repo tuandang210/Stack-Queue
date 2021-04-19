378f9b3551a378d413e314618d63e5dadce57ae7
Dang Tuan <dangminhtuan.arwen@gmail.com>
2021-04-19 15:15:26 +0700

    [Optional] [Thực hành] Triển khai Stack sử dụng mảng

---
+++ MyStack/MyStack.java
@@ -0,0 +1,45 @@
+package MyStack;
+
+public class MyStack {
+    private int[] arr;
+    private int size;
+    private int index;
+
+    public MyStack(int size){
+        this.size=size;
+        arr = new int[size];
+    }
+
+    public void push(int element){
+        if (isFull()) {
+            throw new StackOverflowError("Stack is full");
+        }
+        arr[index] = element;
+        index++;
+    }
+
+    public boolean isFull() {
+        if (index == size) {
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+        return index;
+    }
+
+    public boolean isEmpty() {
+        if (index == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public int pop() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Stack is null");
+        }
+        return arr[--index];
+    }
+}
